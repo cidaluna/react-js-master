d56d3dbc8858cd43aea66783271b2fd7cd14d29a
Cida Luna <cidalunna@hotmail.com>
2024-07-17 14:05:23 -0300

    Ternary operator in JSX code

--- 03-Avancando-no-React/components/ConditionalRender.jsx
+++ 03-Avancando-no-React/components/ConditionalRender.jsx
@@ -3,12 +3,21 @@ import { useState } from 'react';
 const ConditionalRender = () => {
 
     const [x] = useState(true);  // Inicializa com verdade
+    const [name, setName] = useState("Cidoca");
 
   return (
     <div>
       <h1>Isso será exibido ?</h1>
       {x && <p>Se x for true, sim!</p>}  { /* V && V = V */}
       {!x && <p>Agora o x é falso!</p>}  { /* F && F = F */}
+      {name === "Cida Luna" ? (
+        <div>
+          <p>O nome é Cida Luna</p>
+        </div>
+      ) : (
+        <div>Nome não encontrado!</div>
+      ) }
+      <button onClick={() => setName("Cida Luna")}>Reset nome</button>
     </div>
   );
 };
