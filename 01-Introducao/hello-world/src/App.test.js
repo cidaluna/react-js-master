16ecfc4eb924ec1edf5441e71d0f0b9edc2baa57
Cida Luna <cidalunna@hotmail.com>
2024-07-23 10:26:36 -0300

    Unit test in text hello world

--- 01-Introducao/hello-world/src/App.test.js
+++ 01-Introducao/hello-world/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hello World React! title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/Hello World React!/i);
+  expect(textElement).toBeInTheDocument();
 });
