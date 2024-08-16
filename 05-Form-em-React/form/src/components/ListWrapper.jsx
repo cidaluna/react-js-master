e440c252ffe05217a91c94b8e95b479b194a2fbf
Cida Luna <cidalunna@hotmail.com>
2024-08-16 17:06:17 -0300

    Stored the item ID

---
+++ 05-Form-em-React/form/src/components/ListWrapper.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const ListWrapper = ({ items }) => {
+    const [selectedItemId, setSelectedItemId] = useState(undefined);
+
+    const selectedItem = items.find(item => item.id === selectedItemId);
+
+    return (
+      <>
+        Opção selecionada: {selectedItem && <div>{selectedItem.name}</div>}
+        <ul>
+          {items.map(item => (
+            <li
+              key={item.id}
+              onClick={() => setSelectedItemId(item.id)}
+              style={{
+                cursor: 'pointer',
+                backgroundColor: item.id === selectedItemId ? 'darkblue' : 'lightgreen',
+                color: 'magenta',
+                fontSize: '18px',
+                listStyle: 'none',
+              }}
+            >
+              {item.name}
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+}
+
+export default ListWrapper
