0afb807104ccdac3681de8b58d99743a5466338b
Cida Luna <cidalunna@hotmail.com>
2024-09-16 19:22:15 -0300

    Created post and save on database firebase firestore

--- 10-Mini-Blog/miniblog/src/pages/CreatePost/CreatePost.jsx
+++ 10-Mini-Blog/miniblog/src/pages/CreatePost/CreatePost.jsx
@@ -1,9 +1,80 @@
-import styles from './CreatePost.module.css'
+import { useState } from 'react';
+import { useAuthValue } from '../../contexts/AuthContext';
+import { useInsertDocument } from '../../hooks/useInsertDocument';
+import styles from './CreatePost.module.css';
 
 const CreatePost = () => {
+
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
+
+  const { user } = useAuthValue();
+  const { insertDocument, response} = useInsertDocument("posts"); // passando o nome da colection do banco
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError(""); // zerar erros do formulário
+    // validar a url da imagem
+
+    // criar o array de tags
+
+    //checar todos os valores
+
+    insertDocument({
+      title, image, body, tags,
+      uid: user.uid,
+      createdBy: user.displayName
+    });
+
+    // redirect to home page
+
+  };
+  
   return (
-    <div>
-      <h1 className={styles.myPost}>Bora criar um post ?</h1>
+    <div className={styles.myPost}>
+      <h1>Bora criar um post ?</h1>
+      <p>Escreva sobre o que deseja e compartilhe o seu conhecimento.</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Título: </span>
+          <input type="text" name="title" required 
+          placeholder="Pense num bom título... "
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          />
+        </label>
+        <label>
+          <span>URL da imagem: </span>
+          <input type="text" name="image" required 
+          placeholder="Insira uma imagem que represente o seu post "
+          onChange={(e) => setImage(e.target.value)}
+          value={image}
+          />
+        </label>
+        <label>
+          <span>Conteúdo: </span>
+          <textarea name="body" required placeholder="Insira o conteúdo do post"
+          onChange={(e) => setBody(e.target.value)}
+          value={body}
+          ></textarea>
+        </label>
+        <label>
+          <span>Tags: </span>
+          <input type="text" name="tags" required 
+          placeholder="Insira as tags separadas por vírgula "
+          onChange={(e) => setTags(e.target.value)}
+          value={tags}
+          />
+        </label>
+        
+        {!response.loading && <button className="btn">Cadastrar</button>}
+        {response.loading && <button className="btn" disabled>Aguarde...</button>}
+        {response.error && <p className="error">{response.error}</p>}
+        
+      </form>
     </div>
   )
 }
